b655bfd9dad40d49869ca5b940c6747ab7a3816a
Zoli Digness <zdigness@gmail.com>
2024-05-07 15:19:23 -0600

    Update dockerfiles and docker-compose to include volume mounting for client and server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,13 @@ import react from "@vitejs/plugin-react-swc"
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4200,
     host: "0.0.0.0",
+    port: 5173,
+    hmr: {
+      clientPort: 5173,
+    },
+    watch: {
+      usePolling: true,
+    },
   },
 })
